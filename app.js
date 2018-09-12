96e3edd6fc01aeee711383ee21b1ccd57af06bf2
WILL MEIER <willmeierart@gmail.com>
2018-09-12 11:05:50 -0600

    refactor into discrete function, start work on async foreach promiseall for tag replacement

--- app.js
+++ app.js
@@ -1,96 +1,138 @@
 const find = require('find')
 const fs = require('fs')
 const file = require('file')
+const { promisify } = require('util')
 
-const scriptFinder = /(<script async="" id="__NEXT_PAGE|<script src="\/_next)([^;]*?)\>\<\/script\>/g
-const srcFinder = /(?<=src="\/).*?(?=")/
+let newFileName = 'smc_lp_02.html'
+MSify(newFileName)
 
 
-const transformer = async fileNamePlusParentDir => {
-  // use parentDir also because next/static outputs all files as index
+function MSify (newFileName, sourceFileNamePlusParentDir) {
+  const headTagFinder = /\<link rel="preload(.*?)as="script"\/\>/g
+  const scriptFinder = /(<script async="" id="__NEXT_PAGE|<script src="\/_next)([^;]*?)\>\<\/script\>/g
+  const srcFinder = /(?<=src="\/).*?(?=")/
+
+  const outputDir = 'missionsuiteready'
+  // const safeDirname = file.path.abspath(__dirname)
+
   let transformedFileTxt = ''
-  const splitFileName = fileNamePlusParentDir.split('/')
-  const fileName = splitFileName[1]
-  const parentDir = splitFileName[0]
-  // recursively check folders for files that match
-  file.walkSync(__dirname, (walkDirPath, walkDirs, walkFiles) => {
-    const matchedFiles = walkFiles.filter(f => f === fileName)
-    const walkParentDir = walkDirPath.split('/').pop()
-    const isntPageFolder = walkDirPath.indexOf('page') === -1
-    // theres also a page/index file >>> can you use a regex method to clean this sloppy part up?
-    if (matchedFiles.length > 0 && walkParentDir === parentDir && isntPageFolder) {
-      const sourceFile = file.path.join(walkDirPath, matchedFiles.pop())
-      // console.log('source file: ', sourceFile)
-      // get txtContent of original HTML
-      fs.readFile(sourceFile, 'utf8', (err, sourceFileTxt) => {
+
+  const writeNewFile = async newHTML => {
+    const outputFolderAbsPath = file.path.join(__dirname, outputDir)
+    const outputFileAbsPath = file.path.join(outputFolderAbsPath, newFileName).replace(' ', '\ ')
+    if (!fs.existsSync(outputFolderAbsPath)) {
+      try {
+        await fs.mkdirSync(outputFolderAbsPath)
+        fs.writeFileSync(outputFileAbsPath, transformedFileTxt)
+      } catch (e) { console.log(e) }
+    } else {
+      if (fs.existsSync(outputFileAbsPath)) {
+        try {
+          await fs.unlinkSync(outputFileAbsPath)
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      try {
+        fs.writeFileSync(outputFileAbsPath, transformedFileTxt)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
+  const handleExtraTagsFor = txtContent => {
+    let newHTML = txtContent
+    console.log(newHTML, 'WORKED')
+    writeNewFile(newHTML)
+  }
+
+  const replaceRefWithScriptContent = async (scriptFile, sourceFileTxt, scriptMatch, shouldFire) => {
+    // replace script ref tag with inline script
+    try {
+      await fs.readFile(scriptFile, 'utf8', (err, scriptFileTxt) => {
+        if (err) console.log(err)
+        else {
+          transformedFileTxt = sourceFileTxt.replace(scriptMatch, `<script type='text/javascript'>${scriptFileTxt}</script>`)
+          if (shouldFire) {
+            handleExtraTagsFor(transformedFileTxt)
+          }
+        }
+      })
+    } catch (e) { console.log(e) }
+  }
+
+  const findEachScriptFileRef = async (scriptMatches, sourceFileTxt) => {
+    // find each actual script file ref'd inline
+    scriptMatches.forEach((scriptMatch, i) => {
+      const src = new RegExp(scriptMatch.match(srcFinder).pop(), 'g')
+      try {
+        find.file(src, __dirname, scriptFiles => {
+          if (scriptFiles.length > 0) {
+            const scriptFile = scriptFiles.pop()
+            const shouldFire = i === scriptMatches.length - 1
+            replaceRefWithScriptContent(scriptFile, sourceFileTxt, scriptMatch, shouldFire)
+          } else {
+            console.log('no script files found')
+          }
+        })
+      } catch (e) { console.log(e) }
+    })
+  }
+
+  const findEachHeadFileTag = async (headTagMatches, sourceFileTxt) => {
+    headTagMatches.forEach((tagMatch, i) => {
+
+    })
+  }
+
+  const getHTMLtxtContent = async sourceFile => {
+    // get txtContent of original HTML
+    try {
+      await fs.readFile(sourceFile, 'utf8', (err, sourceFileTxt) => {
         if (err) console.log(err)
         else {
           const scriptMatches = sourceFileTxt.match(scriptFinder)
           if (scriptMatches.length > 0) {
-            // find each actual script file ref'd inline
-            scriptMatches.forEach((scriptMatch, i) => {
-              const src = new RegExp(scriptMatch.match(srcFinder).pop(), 'g')
-              find.file(src, __dirname, scriptFiles => {
-                if (scriptFiles.length > 0) {
-                  const scriptFile = scriptFiles.pop()
-                  // console.log('script file found: ', scriptFile)
-
-                  // replace script ref tag with inline script
-                  fs.readFile(scriptFile, 'utf8', (err, scriptFileTxt) => {
-                    if (err) console.log(err)
-                    else {
-                      transformedFileTxt = sourceFileTxt.replace(scriptMatch, `<script type='text/javascript'>${scriptFileTxt}</script>`)
-                      // if (i === scriptMatches.length - 1) return transformedFileTxt
-                    }
-                  })
-                } else console.log('couldnt find the referenced script file')
-              })
-            })
-          } else console.log('no matching script tags found in source file')
+            findEachScriptFileRef(scriptMatches, sourceFileTxt)
+          }
+          const headTagMatches = sourceFileTxt.match(headTagFinder)
+          if (headTagMatches.length > 0) {
+            findEachHeadFileTag(headTagMatches, sourceFileTxt)
+          }
         }
       })
-    }
-  })
-  
-  // await setTimeout(() => { return transformedFileTxt }, 1000)
-  return transformedFileTxt
+    } catch(e) { console.log(e) }
+  }
+
+  const checkFoldersForMatchedFiles = async (fileName, parentDir) => {
+    try {
+      await file.walkSync(__dirname, (walkDirPath, walkDirs, walkFiles) => {
+        // recursively check folders for files that match
+        const matchedFiles = walkFiles.filter(f => f === fileName)
+        const walkParentDir = walkDirPath.split('/').pop()
+        const isntPageFolder = walkDirPath.indexOf('page') === -1
+        // theres also a page/index file >>> can you use a regex method to clean this sloppy part up?
+        // console.log(matchedFiles)
+
+        if (matchedFiles.length > 0 && walkParentDir === parentDir && isntPageFolder) {
+          const sourceFile = file.path.join(walkDirPath, matchedFiles.pop())
+          // console.log(sourceFile)
+          await getHTMLtxtContent(sourceFile)
+        }
+      })
+    } catch (e) { console.log(e) }
+  }
+
+  const transformer = async fileNamePlusParentDir => {
+    // use parentDir also because next/static outputs all files as index
+    const splitFileName = fileNamePlusParentDir.split('/')
+    const fileName = splitFileName[1]
+    const parentDir = splitFileName[0]
+    checkFoldersForMatchedFiles(fileName, parentDir)
+  }
+
+  transformer('index/index.html')
 }
 
-const transformedHTML = transformer('index/index.html')
-
-const handleExtraTagsFor = txtContent => {
-  let newHTML = txtContent
-  return newHTML
-}
-
-const readyHTML = handleExtraTagsFor(transformedHTML)
-
-console.log(readyHTML)
-
-
-const writeNewFile = readyHTML => {
-  find.dir('missionsuiteready', __dirname, (dirs, err) => {
-    if (err) console.log(err)
-    const outputDirExists = dirs.length > 0
-    console.log(outputDirExists)
-    if (outputDirExists) {
-      
-    } else {
-
-    }
-  })
-  
-  // if ()
-}
-
-writeNewFile(readyHTML)
-
-
-// const doIt = async () => {
-//   await readyHTML
-//   console.log(readyHTML)
-  
-  
-// }
-
-// doIt()
+module.exports = MSify
